87fd26b203ce33b1cbb94969d68fd014cc5d7b08
sofaspawn <h.m1nus.work@gmail.com>
2024-12-04 19:23:15 +0530

    working

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,52 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let f = fs::read_to_string("ex.txt").unwrap();
+    let mut wgrid = Vec::new();
+    for line in f.lines() {
+        let mut l = Vec::new();
+        for c in line.chars() {
+            l.push(c);
+        }
+        wgrid.push(l);
+    }
+    dbg!(part1(&wgrid));
+}
+
+fn part1(wgrid: &Vec<Vec<char>>) -> i32 {
+    let mut ans = 0;
+
+    let dirs: Vec<[i32; 2]> = vec![
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+    ];
+
+    let word = "XMAS".to_string();
+
+    for y in 0..wgrid.len() {
+        for x in 0..wgrid[y].len() {
+            let mut idx = 0;
+            if wgrid[y][x] == word.chars().nth(idx).unwrap() {
+                for dir in &dirs {
+                    let i = dir[0];
+                    let j = dir[1];
+                    if y as i32 + j < 0
+                        || y as i32 + j >= wgrid.len() as i32
+                        || x as i32 + i < 0
+                        || x as i32 + i >= wgrid[y].len() as i32
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    ans
 }
